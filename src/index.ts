832ebcc2ee76f2723e7c6eb270d8d0a50c2e507d
YuriiVladimirovichPopov <papanchik87@yahoo.com>
2023-05-15 21:26:06 +0500

    7

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,7 @@ const db: DB = {
     "minAgeRestriction": null,
     "createdAt": new Date().toISOString(),
     "publicationDate": new Date(Date.now() + 1000 * 60 * 60 * 24).toISOString(),
-    "availableResolutions": ["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"]
+    "availableResolutions": []
   },
   {
     "id": 1,
@@ -60,7 +60,7 @@ const db: DB = {
     "minAgeRestriction": null,
     "createdAt": new Date().toISOString(),
     "publicationDate": new Date(Date.now() + 1000 * 60 * 60 * 24).toISOString(),
-    "availableResolutions": ["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"]
+    "availableResolutions": []
   },
 ]
 }
